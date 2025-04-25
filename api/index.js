05544ae1874bb3b37d4bf13548a62c911dfd6029
Abel Tadesse <tadesseabel26@gmail.com>
2025-04-25 09:35:23 +0300

    feat: implement user routing and controller with test endpoint

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -12,6 +14,12 @@ mongoose
   });
 
 const app = express();
+
+app.use("/api/user", userRouter);
+
+app.get("/test", (req, res) => {
+  res.json("Hello World");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000 !!");
