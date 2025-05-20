9c59241f223c52e66eac5f0164f4ea5cd0009bf7
Abel Tadesse <tadesseabel26@gmail.com>
2025-05-20 15:41:49 +0300

    feat: create Home component with introductory text and navigation link to Search page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
+          Find your next <span className="text-slate-500">perfect</span>
+          <br />
+          place with ease
+        </h1>
+        <div className="text-gray-400 text-xs sm:text-sm">
+          Sahand Estate is the best place to find your next perfect place to
+          live.
+          <br />
+          We have a wide range of properties for you to choose from.
+        </div>
+        <Link
+          to={"/search"}
+          className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
+        >
+          Let's get started...
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
